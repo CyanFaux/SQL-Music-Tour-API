918f370a85f260e4d0cc60abbf16877115968478
CyanFaux <santafox26@gmail.com>
2024-03-17 11:51:50 -0400

    sterf

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const { Sequelize } = require('sequelize')
 const app = express()
 
 // CONFIGURATION / MIDDLEWARE
@@ -7,15 +8,23 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.DB_CONNECTION);
+
+try {
+        sequelize.authenticate();
+        console.log(`Connected with Sequelize at ${process.env.DB_CONNECTION}`);
+    } catch (err) {
+        console.error(`Unable to connect to PostgreSQL: ${err}`);
+    }
+
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
     })
 })
-
-
-
+  
 // LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
